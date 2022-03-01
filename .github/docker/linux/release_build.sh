45372f8e78e3c0e298621e0736abcd760b648a30
Kenton Shade, Lead Workflow Engineer <kentonshade@gmail.com>
2022-03-01 09:08:24 -0500

    GHA-131: More build-release testing
    Updating NEW release workflow, small changes to release_build.sh
    
    [Ticket: https://newrelic.atlassian.net/browse/GHA-131]

--- .github/docker/linux/release_build.sh
+++ .github/docker/linux/release_build.sh
@@ -5,7 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-echo php $PHP_VER
-echo arch $ARCH
+# GHA: variables added for local testing
+# PHP_VER='7.0'
+# ARCH='x64'
+echo php "${PHP_VER}"
+echo arch "${ARCH}"
 make -j $(nproc) clean
-make -r -j $(nproc) release-${PHP_VER}-gha "OPTIMIZE=1" "ARCH=${ARCH}"
+make -r -j $(nproc) release-"${PHP_VER}"-gha "OPTIMIZE=1" "ARCH=${ARCH}"
